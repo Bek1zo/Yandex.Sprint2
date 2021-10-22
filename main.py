57b478b12832ae1f7c0833e6daa465c42f74aae0
”Bek1zo” <bekizo@icloud.com>
2021-10-22 10:51:36 +0300

    Added get_week_stats

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 import datetime as dt
 
 class Record():
@@ -13,6 +14,7 @@ class Calculator():
         records = []
         self.records = records
         self.today_summ = 0
+        self.week_summ = 0
         self.currency = ''
 
 
@@ -30,9 +32,9 @@ class CashCalculator(Calculator):
         current_limit = self.limit
         current_today_summ = self.today_summ
         if currency == 'USD':
-            return(current_limit // 70 - current_today_summ // 70)
+            return(current_limit / 70 - current_today_summ / 70)
         elif currency == 'Euro':
-            return(current_limit // 80 - current_today_summ // 80)
+            return(current_limit / 80 - current_today_summ / 80)
         elif currency == 'Rouble':
             return(current_limit - current_today_summ)
 
@@ -40,17 +42,40 @@ class CashCalculator(Calculator):
         if self.today_summ == self.limit:
             print('Денег нет, держись!')
         elif self.today_summ > self.limit:
-                print('Денег нет, держись: твой долг', self.courses(currency), currency)
+                print('Денег нет, держись: твой долг', round(self.courses(currency), 2), currency)
         else:
-                print('На сегодня осталось:', self.courses(currency), currency)
+                print('На сегодня осталось:', round(self.courses(currency), 2), currency)
+
+    def get_week_stats(self):
+        today = dt.date.today()
+        last_week = []
+        #last_week = today - datetime.timedelta(days=7)
+
+        for i in range(7):
+            days = today - datetime.timedelta(days=i)
+            last_week.append((f'{days}'))
+
+
+        for i in range(len(self.records)):
+            if self.records[i][2] in last_week:
+                self.week_summ += self.records[i][0]
+
+        print(self.week_summ)
+        print(last_week)
 
 
 cash_calculator = CashCalculator(1000)
 cash_calculator.add_record(Record(amount=145, comment="кофе"))
 cash_calculator.add_record(Record(amount=150, comment="чай"))
-cash_calculator.add_record(Record(amount=150, comment="пиво", date="2021-10-15"))
+cash_calculator.add_record(Record(amount=180, comment="пиво", date="2021-10-17"))
+cash_calculator.add_record(Record(amount=200, comment="столовая", date="2021-10-18"))
+cash_calculator.add_record(Record(amount=200, comment="кафе", date="2021-10-15"))
 cash_calculator.get_today_stats()
 cash_calculator.get_today_cash_remained('USD')
 cash_calculator.get_today_cash_remained('Euro')
 cash_calculator.get_today_cash_remained('Rouble')
-print(cash_calculator.records)
+cash_calculator.get_week_stats()
+
+
+
+#print(cash_calculator.records)
